7a03bfa28f6c20d844fa3feb68e8fc8892584502
akhmadas25 <88326475+akhmadas25@users.noreply.github.com>
2021-09-09 21:45:33 +0700

    Update 1.js

--- 1.js
+++ 1.js
@@ -1,2 +1,16 @@
-var qts = [A, B, C];
-var qty = []
+function discount(grade, qty ){
+    var total= [];
+    if(grade = "A" && qty>13){
+        
+        total= (4550*qty-(231*qty));
+    }
+    else if(grade = "B" && qty>7){
+        total= (5330*qty-(2.3*(5330*qty)));
+    }
+    else if(grade = "C"){
+        total= (8653*qty);
+    }
+    
+}
+return discount(B, 15);
+console.log(total)
